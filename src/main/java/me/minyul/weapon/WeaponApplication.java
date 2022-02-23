feb90c5be34d75dbc657c5914c441eaad8b5e265
minyul <ggomjae@gmail.com>
2022-02-23 12:34:05 +0900

    init

---
+++ src/main/java/me/minyul/weapon/WeaponApplication.java
@@ -0,0 +1,13 @@
+package me.minyul.weapon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeaponApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeaponApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/minyul/weapon/WeaponApplicationTests.java
@@ -0,0 +1,13 @@
+package me.minyul.weapon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeaponApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

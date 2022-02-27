e9809a38691d2a241eff5ab7b8fb464581fa51f6
민율 <min-yul@min-yul-ui-MacBookAir.local>
2022-02-27 14:36:06 +0900

    test : mac 계정 설정

---
+++ src/main/java/me/minyul/weapon/api/PostController.java
@@ -0,0 +1,6 @@
+package me.minyul.weapon.api;
+
+public class PostController {
+    //
+}
+
---
+++ src/test/java/me/minyul/weapon/api/PostControllerTest.java
@@ -0,0 +1,15 @@
+package me.minyul.weapon.api;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("Test")
+class PostControllerTest {
+
+    @Test
+    void test() {
+        System.out.println("test");
+    }
+}
